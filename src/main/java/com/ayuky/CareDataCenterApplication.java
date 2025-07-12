283ccf9479ad5762bfb6b1af885169c1783c9ff1
Ayuky <3316079442@qq.com>
2025-07-12 20:30:53 +0800

    Initial commit

---
+++ src/main/java/com/ayuky/CareDataCenterApplication.java
@@ -0,0 +1,13 @@
+package com.ayuky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareDataCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareDataCenterApplication.class, args);
+    }
+
+}
